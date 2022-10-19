182bfef4b69bb16ed27f177143aad2fa98b83d41
barnesd8 <barnesd8@msu.edu>
2022-10-19 01:33:50 -0400

    updated class

--- ceREEBerus/reeb.py
+++ ceREEBerus/reeb.py
@@ -1,19 +1,16 @@
 import networkx as nx
 
-class reeb:
+class Reeb:
     """ Class for Reeb Graph
     
     """
 
-    def node_position(self, G):
-        """ Return Node Position for a Reeb Graph
-        
-        """
-        pos = nx.get_node_attributes(G,'pos')
-        return pos
+    def __init__(self, G):
+        self.G = G
+        self.pos = nx.get_node_attributes(self.G,'pos')
     
-    def plot_reeb(self, G, pos):
-        """ Plot a Reeb Graph given a position
+    def plot_reeb(self):
+        """ Plot a Reeb Graph given a graph with a position
         
         """
-        nx.draw(G,pos)
+        nx.draw(self.G,self.pos)
