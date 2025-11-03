e3d49a504bb45049c2aefead1f617b25d75fff2f
ABFritschi <afritschi@unm.edu>
2025-11-03 00:33:08 -0700

    runmapper now preserves f_vertex information

--- cereeberus/cereeberus/reeb/mapper.py
+++ cereeberus/cereeberus/reeb/mapper.py
@@ -3,10 +3,9 @@ from ..distance.labeled_blocks import LabeledBlockMatrix as LBM
 from ..distance.labeled_blocks import LabeledMatrix as LM
 from ..compute.unionfind import UnionFind
 import numpy as np
-import networkx as nx
 
 
-#This is a bad but currently necessary inclusion
+#This is probably bad
 from asteval import Interpreter
 aeval = Interpreter()
 
@@ -292,7 +291,11 @@ class MapperGraph(ReebGraph):
 
     #Creates a list of covers, together with any points inside the cover, ie, [[cover, point1, point2], [cover, point3]]
     #Also removes any covers that have no points inside of them
+    #this would probably be better done as a struct, containing a covering set, the covering set's position in the cover, and an array of points
     def __createcoveringsets(points, cover):
+        #adds location information to cover
+        for val0 in range(len(cover)):
+            cover[val0] = (cover[val0][0], cover[val0][1], val0)
         #creates the list
         coveringsets = []
         for val1 in range(len(cover)):
@@ -315,8 +318,8 @@ class MapperGraph(ReebGraph):
         #trivial clustering
         if clusteralgorithm == "trivial":
             for val in range(len(coveringsets)):
-                coveringsets[val].pop(0)
-                coveringsets[val].insert(0, val)
+                location = coveringsets[val].pop(0)
+                coveringsets[val].insert(0, location[2])
             return coveringsets
         #execute sklearn clusterings
         elif callable(clusteralgorithm):
@@ -331,7 +334,7 @@ class MapperGraph(ReebGraph):
                 cluster_out = clusteralgorithm(coverpointcloud)
                 #puts points into list
                 for val2 in range(max(cluster_out.labels_)+1):
-                    cluster.append([val1])
+                    cluster.append([coveringsets[val1][0][2]])   #The position of the covering set in the cover (preserved for distance purposes)
                     for val3 in range(len(cluster_out.labels_)):
                         if cluster_out.labels_[val3] == val2:
                             cluster[val2].append(coverpointcloud[val3])
@@ -382,10 +385,22 @@ class MapperGraph(ReebGraph):
 
     #function to create covers
     #cover(min, max, #covers, %overlap)
-    def cover(min, max, numcovers, percentoverlap):
+    def cover(min=-1, max=1, numcovers=10, percentoverlap=.5):
+        """
+        Creates a cover to be used for inputs in the runmapper function
+
+        Parameters:
+            min: the minimum for the range of the covering sets
+            max: the maximum for the range of the covering sets
+            numcovers: number of covers to create
+            percentoverlap: percentage (from 0 to 1) of overlap between covers
+
+        Returns:
+            An array of intervals
+        """
         output = []
         val = 0
-        coversize = (max - min)/numcovers * (1+(percentoverlap/100))
+        coversize = (max - min)/numcovers * (1+(percentoverlap))
         while val < numcovers:
             center = (min*(numcovers-(val+0.5)) + max*(val+0.5))/numcovers
             output.append(((-0.5*coversize) + center, (0.5*coversize) + center))
